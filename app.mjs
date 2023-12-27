7bf16d7c0c3f1732be9666ad179c8832be2bfee0
Shayan Ahmad <sa6097@nyu.edu>
2023-12-27 20:11:27 +0500

    add option for other port

--- app.mjs
+++ app.mjs
@@ -1,10 +1,11 @@
-// app.mjs
 import express from 'express';
 const app = express();
-const port = 3000;
+
+// Use the environment variable for the port, if available; otherwise, use port 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public')); // Serve static files from the 'public' directory
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening on port ${port}`);
 });
